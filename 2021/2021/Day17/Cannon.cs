d9f061e657ce1e3292cfb92ecdef3affdd09ce79
SoftAdviceJR <jeroen@softadvice.be>
2021-12-18 14:01:58 +0100

    Day 17

---
+++ 2021/2021/Day17/Cannon.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Submarine.Day17
+{
+	class Cannon
+	{
+		public readonly int targetMinX;
+		public readonly int targetMinY;
+		public readonly int targetMaxX;
+		public readonly int targetMaxY;
+
+		public Cannon(string input)
+		{
+			var digits = new Regex(@"(-?[0-9]*)");
+
+			var matches = digits.Matches(input).Where(m => !string.IsNullOrWhiteSpace(m.Value));
+			if (matches.Count() != 4)
+				throw new InvalidOperationException("Invalid input");
+
+			int x1 = int.Parse(matches.ElementAt(0).Value);
+			int x2 = int.Parse(matches.ElementAt(1).Value);
+
+			int y1 = int.Parse(matches.ElementAt(2).Value);
+			int y2 = int.Parse(matches.ElementAt(3).Value);
+
+			targetMinX = Math.Min(x1, x2);
+			targetMaxX = Math.Max(x1, x2);
+			targetMinY = Math.Min(y1, y2);
+			targetMaxY = Math.Max(y1, y2);
+
+		}
+
+		public bool TryShoot(int vx, int vy)
+		{
+			var landedOn = GetPath(vx, vy).Last();
+
+			return OnTarget(landedOn.Item1, landedOn.Item2);
+		}
+
+		public Tuple<int, int>[] GetPath(int vx, int vy)
+		{
+			int x = 0;
+			int y = 0;
+
+			List<Tuple<int, int>> trajectory = new List<Tuple<int, int>>();
+
+			while(!Overshot(x, y) && !OnTarget(x,y))
+			{
+				x += vx;
+				y += vy;
+				vx = Math.Max(0, vx - 1);
+				vy--;
+				trajectory.Add(new Tuple<int, int>(x, y));
+			}
+
+			return trajectory.ToArray();
+		}
+
+		public Tuple<int, int> VxRange()
+		{
+			int i = 1;
+			while (true)
+			{
+				if (SumOfIntegers(i) >= targetMinX)
+					break;
+				i++;
+			}
+			return new Tuple<int, int>(i, targetMaxX);
+		}
+
+		public Tuple<int, int> VyRange()
+		{
+			int minVy = targetMinY;
+			int maxVy = Math.Abs(minVy) - 1;
+
+			return new Tuple<int, int>(minVy, maxVy);
+		}
+
+		private bool Overshot(int x, int y)
+		{
+			return x > targetMaxX || y < targetMinY;
+		}
+
+		private bool OnTarget(int x, int y)
+		{
+			return x >= targetMinX && x <= targetMaxX && y >= targetMinY && y <= targetMaxY;
+		}
+
+		private static int SumOfIntegers(int n)
+		{
+			return (n * (n + 1)) / 2;
+		}
+
+
+	}
+}
---
+++ 2021/2021/Day17/Solution.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+using Newtonsoft.Json;
+
+using Submarine.Helpers;
+
+namespace Submarine.Day17
+{
+	static class Solution
+	{
+		private static string[] ReadInput()
+		{
+			var lines = File.ReadAllLines("Day17/Input.txt");
+
+			return lines;
+		}
+
+		public static decimal Part1()
+		{
+			var lines = ReadInput();
+
+			var cannon = new Cannon(lines[0]);
+			var vxRange = cannon.VxRange();
+			var vyRange = cannon.VyRange();
+
+
+			var result = cannon.GetPath(vxRange.Item1, vyRange.Item2);
+
+			return result.Max(t => t.Item2);
+		}
+
+		public static long Part2()
+		{
+			var lines = ReadInput();
+
+			var cannon = new Cannon(lines[0]);
+			var vxRange = cannon.VxRange();
+			var vyRange = cannon.VyRange();
+
+			int count = 0;
+
+			for (int vx = vxRange.Item1; vx < vxRange.Item2 + 1; vx++)
+			{
+				for (int vy = vyRange.Item1; vy < vyRange.Item2 + 1; vy++)
+				{
+					if (cannon.TryShoot(vx, vy))
+						count++;
+				}
+			}
+
+			return count;
+		}
+
+		
+
+		
+	}
+
+
+}
