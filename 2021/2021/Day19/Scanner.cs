839420553ac6b34333287ba8a3d5b2b0c25733d3
SoftAdviceJR <jeroen@softadvice.be>
2021-12-20 13:54:32 +0100

    Day 19

---
+++ 2021/2021/Day19/Scanner.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MathNet.Spatial;
+using MathNet.Spatial.Euclidean;
+using MathNet.Spatial.Units;
+
+using Newtonsoft.Json;
+
+namespace Submarine.Day19
+{
+	class Scanner
+	{
+		public int ID { get; }
+
+		public Point3D[] Beacons { get => _beacons.ToArray(); }
+
+		private readonly List<Point3D> _beacons = new List<Point3D>();
+
+		public Scanner(string name)
+		{
+			ID = int.Parse(name.Split(' ')[2]);
+		}
+
+		private Scanner(Point3D[] beacons, int id)
+		{
+			ID = id;
+			_beacons = beacons.ToList();
+		}
+
+		public void AddPoint(string point)
+		{
+			var points = point.Split(',');
+			_beacons.Add(new Point3D(int.Parse(points[0]), int.Parse(points[1]), int.Parse(points[2])));
+		}
+
+		public Scanner Copy()
+		{
+			return new Scanner(Beacons, ID);
+		}
+
+		public Scanner Translate(Vector3D translation)
+		{
+			var beacons = _beacons.Select(b =>
+				 new Point3D(
+					Math.Round(b.X + translation.X),
+					Math.Round(b.Y + translation.Y),
+					Math.Round(b.Z + translation.Z)
+					)
+			).ToArray();
+
+			return new Scanner(beacons, ID);
+		}
+
+		public Scanner Rotate90(Vector3D axis)
+		{
+			var copy = Copy();
+
+			for (int i = 0; i < _beacons.Count; i++)
+			{
+				_beacons[i] = _beacons[i].Rotate(axis, Angle.FromDegrees(90));
+			}
+
+			return copy;
+		}
+
+		public bool TryMatchBeacons(IEnumerable<Point3D> beacons, out Point3D[] transformedBeacons, out Vector3D translation)
+		{
+			if(TryMatch(beacons, out transformedBeacons, out translation))
+			{
+				return true;
+			}
+
+			for (int r = 0; r < Rotations().Length; r++)
+			{
+				var rotation = Rotations()[r];
+				if (Rotate90(rotation).TryMatch(beacons, out transformedBeacons, out translation))
+					return true;
+			}
+
+			transformedBeacons = null;
+			return false;
+		}
+
+		private bool TryMatch(IEnumerable<Point3D> beacons, out Point3D[] transformedBeacons, out Vector3D translation)
+		{
+			for (int i = 0; i < Beacons.Length; i++)
+			{
+				var beacon = Beacons[i];
+
+				for (int j = 0; j < beacons.Count(); j++)
+				{
+					var target = beacons.ElementAt(j);
+
+					translation = beacon.VectorTo(target);
+
+					var translated = Translate(translation);
+
+					if (translated.TwelveBeaconsMatch(beacons))
+					{
+						transformedBeacons = translated.Beacons;
+						
+						return true;
+					}
+				}
+			}
+
+			transformedBeacons = null;
+			translation = Vector3D.NaN;
+			return false;
+		}
+
+
+		public bool TwelveBeaconsMatch(IEnumerable<Point3D> beacons)
+		{
+			var matches = _beacons.Where(b => beacons.Contains(b));
+			if(matches.Count() > 1)
+			{
+
+			}
+			return matches.Count() >= 12;
+		}
+
+
+		private static Vector3D[] Rotations()
+		{
+			return new Vector3D[]
+			{
+				new Vector3D(1,0,0),
+				new Vector3D(1,0,0),
+				new Vector3D(1,0,0),
+
+				new Vector3D(0,0,1),
+
+				new Vector3D(1,0,0),
+				new Vector3D(1,0,0),
+				new Vector3D(1,0,0),
+
+				new Vector3D(0,0,1),
+
+				new Vector3D(1,0,0),
+				new Vector3D(1,0,0),
+				new Vector3D(1,0,0),
+
+				new Vector3D(0,0,-1),
+
+				new Vector3D(1,0,0),
+				new Vector3D(1,0,0),
+				new Vector3D(1,0,0),
+
+				new Vector3D(0,0,-1),
+
+				new Vector3D(1,0,0),
+				new Vector3D(1,0,0),
+				new Vector3D(1,0,0),
+
+				new Vector3D(0,0,1),
+
+				new Vector3D(1,0,0),
+				new Vector3D(1,0,0),
+				new Vector3D(1,0,0),
+			};
+		}
+	}
+}
---
+++ 2021/2021/Day19/Solution.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+using MathNet.Spatial.Euclidean;
+
+using Newtonsoft.Json;
+
+using Submarine.Day2;
+using Submarine.Helpers;
+
+namespace Submarine.Day19
+{
+	static class Solution
+	{
+		private static string[] ReadInput()
+		{
+			var lines = File.ReadAllLines("Day19/Input.txt");
+
+			return lines;
+		}
+
+
+
+		public static decimal Part1()
+		{
+			var lines = ReadInput();
+
+			List<Scanner> scanners = new List<Scanner>();
+
+			Scanner scanner = null;
+
+			for (int i = 0; i < lines.Length; i++)
+			{
+				if (scanner == null)
+				{
+					scanner = new Scanner(lines[i]);
+				}
+				else if (string.IsNullOrWhiteSpace(lines[i]))
+				{
+					scanners.Add(scanner.Copy());
+					scanner = null;
+				}
+				else
+				{
+					scanner.AddPoint(lines[i]);
+				}
+			}
+
+			scanners.Add(scanner);
+
+			List<Point3D> beacons = new List<Point3D>();
+
+			beacons.AddRange(scanners[0].Beacons);
+			scanners.RemoveAt(0);
+
+
+			while (scanners.Any())
+			{
+				for (int i = 0; i < scanners.Count; i++)
+				{
+					if (scanners[i].TryMatchBeacons(beacons, out Point3D[] transformedBeacons, out Vector3D position))
+					{
+						beacons.AddRange(transformedBeacons);
+						beacons = beacons.Distinct().ToList();
+						scanners.RemoveAt(i);
+						break;
+					}
+
+				}
+			}
+
+
+
+
+			return beacons.Count;
+		}
+
+		public static long Part2()
+		{
+			var lines = ReadInput();
+
+			List<Scanner> scanners = new List<Scanner>();
+
+			Scanner scanner = null;
+
+			for (int i = 0; i < lines.Length; i++)
+			{
+				if (scanner == null)
+				{
+					scanner = new Scanner(lines[i]);
+				}
+				else if (string.IsNullOrWhiteSpace(lines[i]))
+				{
+					scanners.Add(scanner.Copy());
+					scanner = null;
+				}
+				else
+				{
+					scanner.AddPoint(lines[i]);
+				}
+			}
+
+			scanners.Add(scanner);
+
+			List<Point3D> beacons = new List<Point3D>();
+
+			beacons.AddRange(scanners[0].Beacons);
+			scanners.RemoveAt(0);
+
+			var positions = new List<Vector3D>();
+
+			while (scanners.Any())
+			{
+				for (int i = 0; i < scanners.Count; i++)
+				{
+					if (scanners[i].TryMatchBeacons(beacons, out Point3D[] transformedBeacons, out Vector3D position))
+					{
+						beacons.AddRange(transformedBeacons);
+						beacons = beacons.Distinct().ToList();
+						scanners.RemoveAt(i);
+						positions.Add(position);
+						break;
+					}
+
+				}
+			}
+
+			return positions.Select(p1 => positions.Max(p2 => ManDist(p1, p2))).Max();
+
+		}
+
+		private static int ManDist(Vector3D p1, Vector3D p2)
+		{
+			return (int)(Math.Abs(p1.X - p2.X) + Math.Abs(p1.Y - p2.Y) + Math.Abs(p1.Z - p2.Z));
+		}
+	}
+
+
+}
