50b8df46f42427aa8387d70410a1e5fc2c8f394d
SoftAdviceJR <jeroen@softadvice.be>
2022-12-01 14:48:21 +0100

    2022 - Day 1

---
+++ 2022/2022/2022/Day1/Part2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022.Day1
+{
+	static class Part2
+	{
+		public static int[][] Read()
+		{
+			List<int[]> results = new List<int[]>();
+
+			var input = File.ReadAllLines("Day1/input.txt");
+
+			List<int> items = new List<int>();
+
+			foreach (var line in input)
+			{
+				if (string.IsNullOrWhiteSpace(line))
+				{
+					results.Add(items.ToArray());
+					items.RemoveAll(d => true);
+					continue;
+				}
+				items.Add(int.Parse(line));
+			}
+
+
+			return results.ToArray();
+		}
+
+		public static int Solve()
+		{
+			var input = Read();
+
+
+			var sums = input.Select(items => items.Sum());
+
+			var ordered = sums.OrderByDescending(s => s);
+			var top3 = ordered.Take(3);
+
+			return top3.Sum();
+		}
+
+		
+	}
+}
---
+++ 2022/2022/2022/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Day 1 - Part 1: " + Day1.Part1.Solve().ToString());
+			Console.WriteLine("Day 1 - Part 2: " + Day1.Part2.Solve().ToString());
+			Console.ReadLine();
+		}
+	}
+}
